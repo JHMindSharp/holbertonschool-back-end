09d2fe7d0c0286b5d422623c9141bdba5a032bb3
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-04-15 10:56:32 +0200

    task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import json
+import requests
+
+
+def export_all_tasks():
+    """Exports tasks for all employees to a
+    JSON file using JSONPlaceholder API."""
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    users_response = requests.get(users_url)
+    todos_response = requests.get(todos_url)
+
+    if users_response.ok and todos_response.ok:
+        users = users_response.json()
+        todos = todos_response.json()
+
+        user_tasks = {}
+        for user in users:
+            user_id = user['id']
+            user_tasks[user_id] = [{
+                "username": user['username'],
+                "task": task['title'],
+                "completed": task['completed']
+            } for task in todos if task['userId'] == user_id]
+
+        with open('todo_all_employees.json', 'w') as jsonfile:
+            json.dump(user_tasks, jsonfile, indent=4)
+
+
+if __name__ == "__main__":
+    export_all_tasks()
